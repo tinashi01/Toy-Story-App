9a306462bd27d43c6bbb2974c7b96728729c6611
Tina Shi <init.defaultBranch>
2023-04-30 16:12:23 +1000

    updat

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Header from './components/Header'
+import ToysList from './components/ToysList';
+import NewToy from './components/NewToy';
+import NoPageFound from './components/NoPageFound'
+import Home from './components/Home'
+import ToyDetail from './components/ToyDetail'
 
 function App() {
+  const [toys, setToys] = useState([]);
+
+    useEffect (() => {
+        fetch('http://localhost:3001/toys')
+        .then(res => res.json())
+        .then (json => {
+            setToys(json)
+        })
+    }, [])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reacts
-        </a>
-      </header>
-    </div>
+    <Router>
+      <React.Fragment>
+      <Header />
+        <Routes>
+          <Route exact path="/" element = {<Home />} />
+          <Route exact path="/toys" element = {<ToysList toys = {toys}/>} />
+          <Route path="/form" element = {<NewToy/>} />
+          <Route path="/toys/:id" element = {<ToyDetail />} />
+          <Route path="/*" element={<NoPageFound /> } />
+        </Routes>
+      </React.Fragment>
+    </Router>
   );
 }
 
